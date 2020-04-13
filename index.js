93b32f8c9c47f358e3e2377872c0ed6fef63097f
Eric Cornetto <putrapurnawa@gmail.com>
2020-04-13 17:09:57 +0800

    alpha version

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const {
+    app,ipcMain,BrowserWindow,Menu
+} = electron;
+Menu.setApplicationMenu(false);
+
+app.on('ready', () => {
+    let home = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: 'Temperature Converter',
+        icon: 'temperature.png'
+    
+    });
+
+    home.loadURL(`file://${__dirname}/index.htm`)
+    home.on('closed', () => {
+        app.quit();
+        home = null;
+    });
+
+});
