28cbe46c2674da8aa0cbe20bf6e7c6a8b8365cd7
ericcornetto <putrapurnawa@gmail.com>
2020-06-05 23:05:01 +0800

    log added

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const electron = require('electron');
 const {
     app,ipcMain,BrowserWindow,Menu
 } = electron;
-Menu.setApplicationMenu(false);
+// Menu.setApplicationMenu(false);
+let home;
 
 app.on('ready', () => {
-    let home = new BrowserWindow({
+    home = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         },
         title: 'Temperature Converter',
-        icon: 'img/temperature.png'
+        icon: 'src/img/temperature.png',
+        width: 800,
+        height: 500
 
     });
 
-    home.loadURL(`file://${__dirname}/index.html`)
+    home.loadURL(`file://${__dirname}/src/layout/index.html`)
     home.on('closed', () => {
         app.quit();
         home = null;
     });
 
 });
+
+ipcMain.on('request:log', event => {
+    home.webContents.send('request:response')
+})
